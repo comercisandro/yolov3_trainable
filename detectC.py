ef86fe0dc1b67e8e8c1e58504362bd8da96d9c5b
sandro.comerci <sandro.comerci@accenture.com>
2022-12-08 16:18:22 -0300

    add poetry

--- detectC.py
+++ detectC.py
@@ -23,9 +23,9 @@ from matplotlib.ticker import NullLocator
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_folder", type=str, default="data/samplesC", help="path to dataset")
-    parser.add_argument("--model_def", type=str, default="config/yolov3-custom3C.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="weights/yolov3_ckpt_198.pth", help="path to weights file")
+    parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
+    parser.add_argument("--model_def", type=str, default="config/yolov3-customRX.cfg", help="path to model definition file")
+    parser.add_argument("--weights_path", type=str, default="weights/yolov3_ckpt_101.pth", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/custom/classes.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
